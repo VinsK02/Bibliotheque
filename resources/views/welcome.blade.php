e76c8f7ee02b71480b2d2e21cf4c7a9b1a93356b
VinsK02 <kevsinini@gmail.com>
2021-07-04 22:38:54 +0000

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+<x-s-i-n-i-n-i_-kevin-layout>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<section class="pt-5 pb-5 mt-0 align-items-center d-flex bg-dark" style="height:100vh; background-size: cover; background-image: url(https://image.freepik.com/vecteurs-libre/bibliotheque-moderne-illustration-etagere_1262-16576.jpg">
+  
+   <div class="container-fluid">
+      <div class="row  justify-content-center align-items-center d-flex text-center h-100">
+        <div class="col-12 col-md-8  h-50 ">
+            <h1 class="display-2  text-light mb-2 mt-5"><strong>BIENVEVU!!</strong> </h1>
+            <p class="lead  text-light mb-5">Lecture à volonté, abondance de connaissance</p>
+<p><a href="https://blueprintsapp.launchaco.com/" class="btn bg-danger shadow-lg btn-round text-light btn-lg btn-rised">Voir plus de livre ></a></p>
+					
+					<div class="btn-container-wrapper p-relative d-block  zindex-1">
+						<a class="btn btn-link btn-lg   mt-md-3 mb-4 scroll align-self-center text-light" href="http://bootstraptor.com">
+						    <i class="fa fa-angle-down fa-4x text-light"></i>
+						    </a>   
+					</div>   
+        </div>
+		 
+      </div>
+    </div>
+    </section>
+    
+
+</x-s-i-n-i-n-i_-kevin-layout>
